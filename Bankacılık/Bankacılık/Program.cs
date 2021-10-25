127973eabd5de0fb8770ab7736f44b03a95994fe
Onur <onur.durgun@hotmail.com>
2021-10-25 18:42:03 +0300

    First Commit

---
+++ Bankacılık/Bankacılık/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bankacılık
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
