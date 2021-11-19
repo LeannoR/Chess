2ca9454e16ca366cd94fef9da8b83b95a5a7405f
gokhandurgun <g9okhan@hotmail.com>
2021-11-20 00:35:07 +0300

    piece color added.

--- Chess/Piece.cs
+++ Chess/Piece.cs
@@ -8,6 +8,14 @@ namespace Chess
 {
     public abstract class Piece
     {
+        public PieceColor Color { get; }
+
+        public Piece(PieceColor color)
+        {
+            this.Color = color;
+        }
+
         public abstract bool CanMove(string from, string to);
+
     }
 }
